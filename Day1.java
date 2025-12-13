1675adb42814bd42f2cad189340e90e841b17ec8
aikhe <ikeandrie.ro@gmail.com>
2025-12-14 02:51:27 +0800

    init: advent2025 (i know im late as fuck but screw it lets do this)

---
+++ Day1.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Day1 {
+	public static String<Array> readFile(String fileName) {
+		File file = new File(fileName);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+
+}
